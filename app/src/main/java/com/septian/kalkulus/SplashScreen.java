e507b3ba2f8cfcaaf3a64c08d10cdcef41b84999
Septian-tri <Septiantriwidian@gmail.com>
2024-04-05 03:07:10 +0700

    [FEATURE] added new popup credits feature

--- app/src/main/java/com/septian/kalkulus/SplashScreen.java
+++ app/src/main/java/com/septian/kalkulus/SplashScreen.java
@@ -1,6 +1,9 @@
 package com.septian.kalkulus;
 
-import android.widget.ImageView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.septian.kalkulus.helper.ImageSetterFromStream;
@@ -11,8 +14,27 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-
+        TextView creditTvClickable = findViewById(R.id.openPopUpCredits);
         ImageSetterFromStream imageSetterFromStream = new ImageSetterFromStream(this);
+
+        creditTvClickable.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LinearLayout popUpCredits = findViewById(R.id.creditsPopUpWrapper);
+                Button closePopUpBtn = findViewById(R.id.closeButtonCredit);
+
+                closePopUpBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        popUpCredits.setVisibility(View.GONE);
+                        creditTvClickable.setVisibility(View.VISIBLE);
+                    }
+                });
+
+                popUpCredits.setVisibility(View.VISIBLE);
+                creditTvClickable.setVisibility(View.GONE);
+            }
+        });
         imageSetterFromStream.setAsImageDrawable("logoUndira.png", R.id.universityLogo);
     }
 }
