20abb7d60e9d51d1e3dc44ee75eb5ca609955bc3
Septian-tri <Septiantriwidian@gmail.com>
2024-04-05 01:42:53 +0700

    init commit

---
+++ app/src/main/java/com/septian/kalkulus/SplashScreen.java
@@ -0,0 +1,18 @@
+package com.septian.kalkulus;
+
+import android.widget.ImageView;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.septian.kalkulus.helper.ImageSetterFromStream;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        ImageSetterFromStream imageSetterFromStream = new ImageSetterFromStream(this);
+        imageSetterFromStream.setAsImageDrawable("undiraLogo.png", R.id.universityLogo);
+    }
+}
